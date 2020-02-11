3c8f5fdfc2bd0b9b5ef15ca9ded7410965c5ccff
rizkydrma <rizky.darmarazak@gmail.com>
2020-02-11 07:02:30 +0700

    membuat fitur delete dan add todo , dan install library uuid, mengubah semua id dengan uuid.v4()

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
+import Header from './components/layout/Header'
 import './App.css';
 import Todos from './components/Todo'
+import AddTodo from './components/AddTodo'
+import uuid from 'uuid';
 
 class App extends Component{
   state ={
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Take out the trash',
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Belajar React',
-        completed: true
+        completed: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Node Js',
         completed: false
       }
@@ -40,10 +43,24 @@ class App extends Component{
   delTodo = (id) => {
     this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]})
   }
+
+  // ADD TODO
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title: title,
+      completed: false
+    }
+    this.setState({todos: [...this.state.todos, newTodo]})
+  }
   render(){
     return(
       <div className="App">
-        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+        <div className="container">
+            <Header />
+            <AddTodo addTodo={this.addTodo} />
+            <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+          </div>
       </div>
     )
   }
