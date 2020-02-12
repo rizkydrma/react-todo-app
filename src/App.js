67da4dc510f29890d8b062273854098b628df89d
rizkydrma <rizky.darmarazak@gmail.com>
2020-02-12 07:25:55 +0700

    membuat pages about dan menggunakan Link dari react router dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/layout/Header'
 import './App.css';
 import Todos from './components/Todo'
 import AddTodo from './components/AddTodo'
+import About from './components/pages/about'
 import uuid from 'uuid';
 
 class App extends Component{
@@ -55,13 +57,21 @@ class App extends Component{
   }
   render(){
     return(
-      <div className="App">
-        <div className="container">
+      <Router>  
+        <div className="App">
+          <div className="container">
             <Header />
-            <AddTodo addTodo={this.addTodo} />
-            <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo} />
+                <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+              </React.Fragment>
+            )} />
+
+            <Route path="/about" component={About} />
           </div>
-      </div>
+        </div>
+      </Router>
     )
   }
 }
---
+++ src/components/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function about() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is a todolist app version v1.0.0. it is part of react</p>
+    </React.Fragment>
+  )
+}
